840e94ef40c119c776eaa8659dfce560adcf7a87
ketjj <jorjolianiket@gmail.com>
2022-04-29 10:42:20 +0200

    update jsnack3

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,18 +1,15 @@
 // Jsnack 3
 // Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
 
-function reverseStringa(str){
-  
-  let reverseString = '';
 
-  reverseString = str.split('').reverse().join('') 
 
-  for(let i = str.length - 1; i >= 0; i--){
-    reverseString += str[i];
-  }
 
-  return reverseString;
-}
 
+// function reverseStringa(str){
+//   return str.split('').reverse().join('');
+// }
+
+const parola = 'ciao'
 
 const reversestringa = (str) => str.split('').reverse().join('');
+console.log(reversestringa(parola));
